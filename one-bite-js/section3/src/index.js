3d64ceddb28d623bef4e301995d0c5b5eb952968
woojung007 <eeeeve007@gmail.com>
2024-08-10 12:40:32 +0900

    ✨ Feat: random color library 사용하기

--- one-bite-js/section3/src/index.js
+++ one-bite-js/section3/src/index.js
@@ -4,6 +4,11 @@
 // esmodule 방식 -> 확장자 필수!! 🚨
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+import randomColor from "randomcolor";
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
+
+const color = randomColor();
+console.log(color);
